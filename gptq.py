90b0ffc6ffb122f3dc0493b4fe48fc793cdff976
jakedahn <jake@replicate.com>
2023-08-31 19:33:14 +0000

    pushing for sylvie

---
+++ gptq.py
@@ -0,0 +1,36 @@
+from transformers import AutoTokenizer, pipeline, logging
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+
+model_path = './CodeLlama-34B-Instruct-GPTQ'
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(model_path,
+        use_safetensors=True,
+        trust_remote_code=False,
+        device="cuda:0",
+        use_triton=False,
+        quantize_config=None,
+        disable_exllama=True,
+        inject_fused_attention=False
+)
+
+prompt_template=f'''[INST] <<SYS>>
+All responses must be written in TypeScript.
+<<SYS>>
+
+Write a function that sums 2 integers together and returns the results.
+[/INST]
+'''
+
+print("\n\n*** Generate:")
+
+input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+
+inference_kwargs = dict(inputs=input_ids, temperature=0.7, max_new_tokens=128, top_p=0.95, top_k=50, do_sample=True)
+print('#### inference params ####')
+for k,v in inference_kwargs.items():
+    print(f'{k}: {v}')
+print('########')
+output = model.generate(**inference_kwargs)
+print(tokenizer.decode(output[0]))
---
+++ yolo.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import glob 
+import torch 
+import time
+import json
+import typing as tp 
+# from src.exllama_predictor import ExllamaGenerator
+
+from exllama.model import ExLlama, ExLlamaCache, ExLlamaConfig
+from exllama.tokenizer import ExLlamaTokenizer
+from exllama.generator import ExLlamaGenerator
+
+model_directory = './models/codellama-34b-instruct/model_artifacts/default_inference_weights'
+# generator = ExllamaGenerator(weights)
+
+tokenizer_path = os.path.join(model_directory, "tokenizer.model")
+model_config_path = os.path.join(model_directory, "config.json")
+st_pattern = os.path.join(model_directory, "*.safetensors")
+model_path = glob.glob(st_pattern)[0]
+with open(model_config_path, 'r') as f:
+    base_model_config = json.load(f)
+    if "max_position_embeddings" in base_model_config:
+        max_seq_len = base_model_config["max_position_embeddings"]
+    else:
+        max_seq_len = 4096
+
+
+config = ExLlamaConfig(model_config_path)               # create config from config.json
+config.model_path = model_path                          # supply path to model weights file
+
+# Override exllam's default settings to use full llama v2 context
+# config.max_seq_len = max_seq_len
+config.max_seq_len = max_seq_len
+config.max_input_len = max_seq_len
+config.max_attention_size = max_seq_len**2
+
+config.repetition_penalty = 1.15
+config.repetition_penalty_sustain = 256
+config.token_repetition_penalty_decay = 128
+config.temperature = 0.95
+config.top_p = 0.65
+config.top_k = 20
+config.max_new_tokens = 128
+config.min_new_tokens = 0
+config.beams = 1
+config.beam_length = 1
+
+model = ExLlama(config)                                 # create ExLlama instance and load the weights
+tokenizer = ExLlamaTokenizer(tokenizer_path)            # create tokenizer from tokenizer model file
+
+
+#FIXME: make sure this cache isnt passing statefulness
+cache = ExLlamaCache(model)                             # create cache for inference
+generator = ExLlamaGenerator(model, tokenizer, cache)   # create generator
+
+
+prompt = '''[INST] <<SYS>>
+All responses must be written in TypeScript.
+<</SYS>>
+
+Write a function that sums 2 integers together and returns the results.
+[/INST]'''
+
+
+def run():
+    # for decoded_token in generator(
+    #     prompt,
+    #     repetition_penalty=1.15,
+    #     repetition_penalty_sustain=256,
+    #     token_repetition_penalty_decay=128,
+    #     temperature=0.6,
+    #     top_p=0.95,
+    #     top_k=50,
+    #     max_new_tokens=512,
+    #     min_new_tokens=-1,
+    #     stop_sequences=None):
+    #         print(decoded_token, end="")
+
+    print(generator.generate_simple(prompt, 512))
+if __name__ == "__main__":
+    run()
