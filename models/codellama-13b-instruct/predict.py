0668eea0e5dcca29e3ab57cddd738edcdf1ec93d
jakedahn <jake@replicate.com>
2023-09-05 20:35:52 +0000

    codellama-13b-instruct, now powered by VLLM

--- models/codellama-13b-instruct/predict.py
+++ models/codellama-13b-instruct/predict.py
@@ -1,43 +1,39 @@
-import shutil
-import time
-from typing import Optional
-import zipfile
-import glob
-import time 
-
+import os
 import torch
+import asyncio
+
+from typing import Optional
+from vllm import AsyncLLMEngine
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.sampling_params import SamplingParams
+
 from cog import BasePredictor, ConcatenateIterator, Input, Path
+
+from transformers import LlamaForCausalLM
+
+from transformers.models.code_llama.tokenization_code_llama_fast import CodeLlamaTokenizerFast
 
 from config import (
     LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH, 
+    LOCAL_TRAINING_WEIGHTS_PATH,
+    REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD, 
     REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH,
     REMOTE_TRAINING_FILES_TO_DOWNLOAD,
-    USE_EXLLAMA_FOR_UNTRAINED_WEIGHTS,
-    REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD, 
-    LOCAL_TRAINING_WEIGHTS_PATH, 
-    REMOTE_TRAINING_WEIGHTS_PATH,
-    LOAD_IN_4BIT,
-    load_tokenizer, 
-    load_tensorizer, 
-    download_file,
     USE_SYSTEM_PROMPT
 )
 
-from subclass import YieldingLlama
 from src.utils import maybe_download_with_pget
 
-from peft import PeftModel
-import os
 
-# This prompt formatting was copied from the original Llama v2 repo:
+# This prompt formatting was copied from the original CodeLlama repo:
 # https://github.com/facebookresearch/llama/blob/6c7fe276574e78057f917549435a2554000a876d/llama/generation.py#L44
 
 # These are components of the prompt that should not be changed by the users
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-PROMPT_TEMPLATE = f"{B_INST} {B_SYS}{{system_prompt}}{E_SYS}{{instruction}} {E_INST}"
+PROMPT_TEMPLATE = f"{B_INST} {{instruction}} {E_INST}"
+PROMPT_TEMPLATE_WITH_SYSTEM_PROMPT = f"{B_INST} {B_SYS}{{system_prompt}}{E_SYS}{{instruction}} {E_INST}"
 
-# Users may want to change the system prompt, but we use the recommended system prompt by default
 DEFAULT_SYSTEM_PROMPT = """"""
 
 
@@ -45,10 +41,10 @@ class Predictor(BasePredictor):
     def setup(self, weights: Optional[Path] = None):
         print('starting setup')
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        
+
         if weights is not None and weights.name == "weights":
-            # bugfix
             weights = None
+
         # If weights aren't passed in, we'll use the default weights configuration
         if not weights:
             weights = LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH
@@ -56,217 +52,131 @@ class Predictor(BasePredictor):
                 weights, REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH, REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD,
             )
 
-            if USE_EXLLAMA_FOR_UNTRAINED_WEIGHTS:
-                from src.exllama_predictor import ExllamaGenerator
-                self.generator = ExllamaGenerator(weights)
-                self.use_exllama = True
-            
-            else:
-                if os.path.isdir(weights):
-                    self.model = self.load_huggingface_model(weights, load_in_4bit=LOAD_IN_4BIT)
-                    self.tokenizer = load_tokenizer()
-                    self.use_exllama = False
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
-        
-        # If weights are passed in, they are LoRa weights
-        # so we need to download the fp16 weights and load with peft
-        elif '.zip' in str(weights):
-            weights = str(weights)
-            self.model = self.load_peft(weights)
-            self.tokenizer = load_tokenizer()
-            self.use_exllama = False
-        else:
-            raise Exception(f"Fine-tuned weights {weights} were improperly formatted.")
-
-
-    def load_peft(self, weights):
-        st = time.time()
-
-        model_path = maybe_download_with_pget(
-            LOCAL_TRAINING_WEIGHTS_PATH, 
-            REMOTE_TRAINING_WEIGHTS_PATH, 
-            REMOTE_TRAINING_FILES_TO_DOWNLOAD,
+        args = AsyncEngineArgs(
+            model=LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH,
+            tokenizer=LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH,
+            dtype="float16",
+            max_num_seqs=16384,
         )
+        self.engine = AsyncLLMEngine.from_engine_args(args)
+        self.tokenizer = self.engine.engine.tokenizer
 
-        model = self.load_huggingface_model(model_path, load_in_4bit=LOAD_IN_4BIT)
-        if 'http' in weights: # weights are in the cloud
-            local_weights = 'local_weights.zip'
-            if not os.path.exists(local_weights):
-                download_file(weights, local_weights)
-            weights = local_weights
-        out = '/src/peft_dir'
-        if os.path.exists(out):
-            shutil.rmtree(out)
-        with zipfile.ZipFile(weights, 'r') as zip_ref:
-            zip_ref.extractall(out)
-        model = PeftModel.from_pretrained(model, out)
-        print(f"peft model loaded in {time.time() - st}")
-        return model.to('cuda')
+    async def generate_stream(
+        self,
+        prompt,
+        temperature=0.6,
+        top_p=0.95,
+        top_k=50,
+        max_new_tokens=128,
+        stop_str=None,
+        stop_token_ids=None,
+        repetition_penalty=1.0
+    ):
+        context = prompt
+        stop_token_ids = stop_token_ids or []
+        stop_token_ids.append(self.tokenizer.eos_token_id)
 
-    def load_huggingface_model(self, weights=None, load_in_4bit=False):
-        st = time.time()
-        print(f"loading weights from {weights} w/o tensorizer")
-        if LOAD_IN_4BIT:
-            model = YieldingLlama.from_pretrained(
-                weights, 
-                cache_dir="pretrained_weights", 
-                device_map='auto',
-                load_in_4bit=LOAD_IN_4BIT,
-            )
+        if isinstance(stop_str, str) and stop_str != "":
+            stop = [stop_str]
+        elif isinstance(stop_str, list) and stop_str != []:
+            stop = stop_str
         else:
-            model = YieldingLlama.from_pretrained(
-                weights, cache_dir="pretrained_weights", torch_dtype=torch.float16, low_cpu_mem_usage=True
-            ).to(self.device)
+            stop = []
 
-        print(f"weights loaded in {time.time() - st}")
-        return model
+        for tid in stop_token_ids:
+            stop.append(self.tokenizer.decode(tid))
+
+        # make sampling params in vllm
+        top_p = max(top_p, 1e-5)
+        if temperature <= 1e-5:
+            top_p = 1.0
+        sampling_params = SamplingParams(
+            n=1,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            use_beam_search=False,
+            stop=stop,
+            max_tokens=max_new_tokens,
+            frequency_penalty=repetition_penalty,
+        )
+        results_generator = self.engine.generate(context, sampling_params, 0)
+
+        async for request_output in results_generator:
+            prompt = request_output.prompt
+            yield request_output.outputs[-1].text
 
     def predict(
         self,
-        prompt: str = Input(description=f"Prompt to send to Codellama"),
+        prompt: str = Input(description=f"Prompt to send to CodeLlama."),
         system_prompt: str = Input(
-            description="System prompt to send to Codellama. This is prepended to the prompt and helps guide system behavior.", 
+            description="System prompt to send to CodeLlama. This is prepended to the prompt and helps guide system behavior.", 
             default=DEFAULT_SYSTEM_PROMPT,
         ),
         max_new_tokens: int = Input(
             description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
             ge=1,
-            default=500,
-        ),
-        min_new_tokens: int = Input(
-            description="Minimum number of tokens to generate. To disable, set to -1. A word is generally 2-3 tokens.",
-            ge=-1,
-            default=-1,
+            default=128,
         ),
         temperature: float = Input(
             description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.",
             ge=0.01,
             le=5,
-            default=0.95,
+            default=0.75,
         ),
         top_p: float = Input(
             description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens",
             ge=0.0,
             le=1.0,
-            default=0.95,
+            default=0.9,
         ),
         top_k: int = Input(
             description="When decoding text, samples from the top k most likely tokens; lower to ignore less likely tokens",
             ge=0,
-            default=250,
+            default=50,
         ),
-        repetition_penalty: float = Input(
-            description="Penalty for repeated words in generated text; 1 is no penalty, values greater than 1 discourage repetition, less than 1 encourage it.",
-            ge=0.01,
-            le=5,
-            default=1.15,
-        ),
-        repetition_penalty_sustain: int = Input(
-            description="Number of most recent tokens to apply repetition penalty to, -1 to apply to whole context",
-            ge=-1,
-            default=256,
-        ),
-        token_repetition_penalty_decay: int = Input(
-            description="Gradually decrease penalty over this many tokens",
-            ge=1,
-            default=128,
+        stop_sequences: str = Input(
+            description="A comma-separated list of sequences to stop generation at. For example, '<end>,<stop>' will stop generation at the first instance of 'end' or '<stop>'.",
+            default=None,
         ),
         debug: bool = Input(
             description="provide debugging output in logs", default=False
         ),
-    ) -> ConcatenateIterator: 
+    ) -> ConcatenateIterator[str]:
         
-        if USE_SYSTEM_PROMPT:
-            prompt = prompt.strip('\n').lstrip(B_INST).rstrip(E_INST).strip()
-            prompt_templated = PROMPT_TEMPLATE.format(system_prompt=system_prompt.strip(), instruction=prompt.strip())
-        else:
-            prompt_templated = prompt
+        if stop_sequences:
+            stop_sequences = stop_sequences.split(",")
+        
+        user_prompt = prompt.strip('\n').lstrip(B_INST).rstrip(E_INST).strip()
+        prompt_templated = PROMPT_TEMPLATE.format(instruction=user_prompt)
+
+        # If USE_SYSTEM_PROMPT is True, and the user has supplied some sort of system prompt, we add it to the prompt.
+        if USE_SYSTEM_PROMPT and system_prompt != '':
+            prompt_templated = PROMPT_TEMPLATE_WITH_SYSTEM_PROMPT.format(system_prompt=system_prompt, instruction=user_prompt)
+
         print(f"Prompt: \n{prompt_templated}")
-        if self.use_exllama:
-            n_tokens = 0
-            st = time.time()
-            for decoded_token in self.generator(
-                prompt_templated,
-                repetition_penalty=repetition_penalty,
-                repetition_penalty_sustain=repetition_penalty_sustain,
-                token_repetition_penalty_decay=token_repetition_penalty_decay,
-                temperature=temperature,
-                top_p=top_p,
-                top_k=top_k,
-                max_new_tokens=max_new_tokens,
-                min_new_tokens=min_new_tokens,
-            ):
-                n_tokens += 1
-                yield decoded_token
-            t = time.time() - st
-            print(f" ** Speed: {n_tokens / t:.2f} tokens/second")
-        
-        # This is our original generation code
-        else:
 
-            input = self.tokenizer(prompt_templated, return_tensors="pt").input_ids.to(self.device)
-            n_in_tokens = input.shape[-1]
-            if n_in_tokens >= self.model.config.max_position_embeddings:
-                raise ValueError(f"Your input is too long. Max input length is {self.model.config.max_position_embeddings} tokens, but you supplied {n_in_tokens} tokens.")
+        loop = asyncio.get_event_loop()
 
-            max_new_tokens = min(max_new_tokens, self.model.config.max_position_embeddings - n_in_tokens)
+        gen = self.generate_stream(
+            prompt_templated,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            max_new_tokens=max_new_tokens,
+            stop_str=stop_sequences,
+            repetition_penalty=1.0
+        )
 
-            with torch.inference_mode() and torch.autocast("cuda"):
-                first_token_yielded = False
-                prev_ids = []
-
-                for output in self.model.generate(
-                    input_ids=input,
-                    max_new_tokens=max_new_tokens,
-                    do_sample=True,
-                    temperature=temperature,
-                    top_p=top_p,
-                    repetition_penalty=repetition_penalty,
-                ):
-                    cur_id = output.item()
-
-
-                    # in order to properly handle spaces, we need to do our own tokenizing. Fun!
-                    # we're building up a buffer of sub-word / punctuation tokens until we hit a space, and then yielding whole words + punctuation.
-                    cur_token = self.tokenizer.convert_ids_to_tokens(cur_id)
-
-                    # skip initial newline, which this almost always yields. hack - newline id = 13.
-                    if not first_token_yielded and not prev_ids and cur_id in [13, 259]:
-                        continue
-
-                    # underscore means a space, means we yield previous tokens
-                    if cur_token.startswith("▁"):  # this is not a standard underscore.
-                        # first token
-                        if not prev_ids:
-                            prev_ids = [cur_id]
-                            continue
-
-                        # there are tokens to yield
-                        else:
-                            token = ' ' + self.tokenizer.decode(prev_ids)
-                            prev_ids = [cur_id]
-
-                            if not first_token_yielded:
-                                # no leading space for first token
-                                token = token.strip()
-                                first_token_yielded = True
-                            yield token
-                    else:
-                        prev_ids.append(cur_id)
-                        continue
-
-                # remove any special tokens such as </s>
-                token = ' ' + self.tokenizer.decode(prev_ids, skip_special_tokens=True).rstrip('\n')
-                if not first_token_yielded:
-                    # no leading space for first token
-                    token = token.strip()
-                    first_token_yielded = True
-                yield token 
-
-        if debug:
-            print(f"cur memory: {torch.cuda.memory_allocated()}")
-            print(f"max allocated: {torch.cuda.max_memory_allocated()}")
-            print(f"peak memory: {torch.cuda.max_memory_reserved()}")
-
-
-
+        prv_value = ""
+        value = ""
+        while True:
+            prv_value = value
+            try:
+                value = loop.run_until_complete(gen.__anext__())
+                yield value[len(prv_value) :]
+            except StopAsyncIteration:
+                break
