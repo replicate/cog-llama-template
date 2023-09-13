8db011fda683734ced8c30211e5a537c128eb8ee
dan <dan@replicate.com>
2023-09-13 22:59:29 +0000

    wip engine

---
+++ src/inference_engines/engine.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Engine(ABC):
+    """
+    WIP - this is what the engine looks like at the moment, outlining this just as an exercise to see what our ABC looks like. It will change. 
+    """
+
+    @abstractmethod
+    @staticmethod
+    def load_lora(self, lora_):
+        """
+        loads a lora from files into the format that this particular engine expects. DOES NOT prepare the engine for inference.
+        """
+        pass
+
+    @abstractmethod
+    def set_lora(self, lora):
+        """
+        given a loaded lora (created w/load_lora), configures the engine to use that lora in combination with the loaded base weights. 
+        """
+
+    @abstractmethod
+    def __call__(self, prompt, **kwargs):
+        """
+        generation!
+        """
+        pass
+
+        
