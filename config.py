64fc71057303b8b854c779713a8583a9da468748
Moin Nadeem <moin@replicate.com>
2023-09-26 01:21:02 +0000

    putting model configuration in the right directory

--- config.py
+++ config.py
@@ -1,90 +1,87 @@
-import os
-import subprocess
-from collections import OrderedDict
-
-import torch
 from dotenv import load_dotenv
-from transformers import AutoConfig, LlamaForCausalLM, LlamaTokenizer
-
 from src.utils import get_env_var_or_default
 
 load_dotenv()
 
-MODEL_NAME = "llama-2-7b"
+MODEL_NAME = "llama-2-7b-chat"
 # INFERENCE CONFIGURATION
 #######################################################################
 # --------------------Notes--------------------------------------------
-# We sometimes implement inference differently for models that have not
-# been trained/fine-tuned vs. those that have been trained/fine-tuned. We refer to the
-# former as "default" and the latter as "trained". Below, you can
-# set your "default inference configuration" and your "trained
-# inference configuration".
+# We are trying our very best to no longer have different inference code paths
+# for trained and untrained weights :)
 #
-# GENERAL INFERENCE CONFIGURATION
+# INFERENCE CONFIGURATION
 # -------------------------------
 # This section defines the general inference configuration,
 # which is used for both trained and untrained models.
 # -------------------------------
 
-LOAD_IN_4BIT = False
-TOKENIZER_PATH = f"models/{MODEL_NAME}/model_artifacts/default_inference_weights"
-USE_SYSTEM_PROMPT = False
-USE_EXLLAMA_FOR_UNTRAINED_WEIGHTS = False
+TOKENIZER_PATH = f"models/{MODEL_NAME}/model_artifacts/tokenizer"
+USE_SYSTEM_PROMPT = True
 
 
-# DEFAULT INFERENCE CONFIGURATION
+# ENGINE CONFIGURATION
 # -------------------------------
-# This section defines the default inference configuration, which may differ from
-# how we implement inference for a trained model.
+# Here we define the specific inference engine we intend to use for inference, and all appropriate kwargs.
 # -------------------------------
 
+from src.inference_engines.exllama import ExllamaEngine
 
-LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH = f"models/{MODEL_NAME}/model_artifacts/default_inference_weights"
+ENGINE = ExllamaEngine
+ENGINE_KWARGS = {
+    "fused_attn": True,
+}
 
-REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH = "https://pub-df34620a84bb4c0683fae07a260df1ea.r2.dev/Llama-2-7b/"
+# WEIGHTS CONFIGURATION
+# -------------------------------
+# Which base weights do we use for inference with this model?
+# -------------------------------
+LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH = (
+    f"models/{MODEL_NAME}/model_artifacts/default_inference_weights"
+)
 
-N_SHARDS = 3
-REMOTE_TRAINING_FILES_TO_DOWNLOAD = [
-    f"model-{str(i+1).zfill(5)}-of-{str(N_SHARDS).zfill(5)}.safetensors"
-    for i in range(N_SHARDS)
-]
+REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH = get_env_var_or_default(
+    "REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH",
+    "remote/path/to/your/weights/here",
+)
 
-# REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD = ["model.safetensors"]
-# N_SHARDS=3
-# REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD = [
-# f"pytorch_model-{str(i+1).zfill(5)}-of-{str(N_SHARDS).zfill(5)}.bin"
-# for i in range(N_SHARDS)
+# N_SHARDS = 2
+# REMOTE_TRAINING_FILES_TO_DOWNLOAD = [
+#     f"model-{str(i+1).zfill(5)}-of-{str(N_SHARDS).zfill(5)}.safetensors"
+#     for i in range(N_SHARDS)
 # ]
 
-REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD = [
-    "checklist.chk",
+REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD = ["gptq_model-4bit-32g.safetensors"]
+
+REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD += [
     "config.json",
-    "model.safetensors.index.json",
-    "params.json",
+    "generation_config.json",
+    "special_tokens_map.json",
+    "tokenizer_config.json",
+    "tokenizer.json",
     "tokenizer.model",
-    "tokenizer_checklist.chk",
-    "consolidated.00.pth",
-    "model-00001-of-00002.safetensors",
-    "model-00002-of-00002.safetensors",
+    "quantize_config.json",
 ]
 
-# TRAINED INFERENCE CONFIGURATION
+# TRAINING CONFIGURATION
 # -------------------------------
-# This section defines the inference configuration for fine-tuned models
+# This section defines the configuration for weights used to train/fine-tune models
 # -------------------------------
 
 LOCAL_TRAINING_WEIGHTS_PATH = f"models/{MODEL_NAME}/model_artifacts/training_weights"
 
 REMOTE_TRAINING_WEIGHTS_PATH = get_env_var_or_default(
     var_name="REMOTE_TRAINING_WEIGHTS_PATH",
-    default_value="remote/path/to/your/weights/here"
+    default_value="remote/path/to/your/weights/here",
 )
 
-LOCAL_TRAINING_WEIGHTS_CONFIG_PATH = f"models/{MODEL_NAME}/model_artifacts/training_weights/config.json"
+LOCAL_TRAINING_WEIGHTS_CONFIG_PATH = (
+    f"models/{MODEL_NAME}/model_artifacts/training_weights/config.json"
+)
 
 REMOTE_TRAINING_WEIGHTS_CONFIG_PATH = get_env_var_or_default(
     var_name="REMOTE_TRAINING_WEIGHTS_CONFIG_PATH",
-    default_value="remote/path/to/your/weights/here"
+    default_value="remote/path/to/your/weights/here",
 )
 
 N_SHARDS = 2
@@ -96,54 +93,9 @@ REMOTE_TRAINING_FILES_TO_DOWNLOAD = [
 REMOTE_TRAINING_FILES_TO_DOWNLOAD += [
     "config.json",
     "generation_config.json",
+    "model.safetensors.index.json",
     "special_tokens_map.json",
     "tokenizer_config.json",
     "tokenizer.json",
     "tokenizer.model",
-    "model.safetensors.index.json"
 ]
-
-
-# -------------------------------
-
-DEFAULT_PAD_TOKEN = "[PAD]"
-DEFAULT_EOS_TOKEN = "</s>"
-DEFAULT_BOS_TOKEN = "<s>"
-DEFAULT_UNK_TOKEN = "</s>"
-
-
-def log_memory_stuff(prompt=None):
-    """One method to barf out everything we'd ever want to know about memory"""
-
-    if prompt is not None:
-        print(prompt)
-    os.system("nvidia-smi")
-    print(torch.cuda.memory_summary())
-
-
-def load_tokenizer():
-    """Same tokenizer, agnostic from tensorized weights/etc"""
-    tok = LlamaTokenizer.from_pretrained(
-        TOKENIZER_PATH, cache_dir="pretrained_weights", legacy=False)
-    tok.add_special_tokens(
-        {
-            "eos_token": DEFAULT_EOS_TOKEN,
-            "bos_token": DEFAULT_BOS_TOKEN,
-            "unk_token": DEFAULT_UNK_TOKEN,
-            "pad_token": DEFAULT_PAD_TOKEN,
-        }
-    )
-    return tok
-
-
-def download_file(file, local_filename):
-    print(f"Downloading {file} to {local_filename}")
-    if os.path.exists(local_filename):
-        os.remove(local_filename)
-    if '/' in local_filename:
-        if not os.path.exists(os.path.dirname(local_filename)):
-            os.makedirs(os.path.dirname(local_filename), exist_ok=True)
-
-    command = ['pget', file, local_filename]
-    subprocess.check_call(command)
-    return
---
+++ models/llama-2-7b-vllm/predict.py
@@ -0,0 +1,151 @@
+import asyncio
+import functools
+import io
+import time
+import zipfile
+from typing import Optional, Tuple
+
+import torch
+from cog import BasePredictor, ConcatenateIterator, Input, Path
+
+from config import (LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH,
+                    REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD,
+                    REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH)
+from src.download import Downloader
+from src.inference_engines.vllm_engine import vLLMEngine
+from src.utils import maybe_download_with_pget
+
+# This prompt formatting was copied from the original CodeLlama repo:
+# https://github.com/facebookresearch/llama/blob/6c7fe276574e78057f917549435a2554000a876d/llama/generation.py#L44
+
+
+class Predictor(BasePredictor):
+    def setup(self, weights: Optional[Path] = None):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        if weights is not None and weights.name == "weights":
+            weights = None
+
+        # If weights aren't passed in, we'll use the default weights configuration
+        if not weights:
+            weights = LOCAL_DEFAULT_INFERENCE_WEIGHTS_PATH
+            local_weights_path = maybe_download_with_pget(
+                weights, REMOTE_DEFAULT_INFERENCE_WEIGHTS_PATH, REMOTE_DEFAULT_INFERENCE_FILES_TO_DOWNLOAD,
+            )
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        self.engine = vLLMEngine(
+            model_path=local_weights_path, tokenizer_path=local_weights_path, dtype="float16")
+        self.tokenizer = self.engine.tokenizer
+        self.downloader = Downloader()
+
+    @functools.lru_cache(maxsize=10)
+    def get_lora(self, replicate_weights: str) -> Tuple[str, io.BytesIO]:
+        if "http" in str(replicate_weights):  # weights are in the cloud
+            print("Downloading peft weights")
+            st = time.time()
+            buffer = self.downloader.sync_download_file(str(replicate_weights))
+            print(f"Downloaded peft weights in {time.time() - st:.3f}")
+        else:
+            # zipfile accepts either a file-like or path-like object
+            buffer = replicate_weights
+        st = time.time()
+        with zipfile.ZipFile(buffer, "r") as zip_ref:
+            data = {name: zip_ref.read(name) for name in zip_ref.namelist()}
+        print(f"Unzipped peft weights in {time.time() - st:.3f}")
+        adapter_model_bytes = io.BytesIO(data["adapter_model.bin"])
+        adapter_config_bytes = data["adapter_config.json"]
+        return (adapter_config_bytes, adapter_model_bytes)
+
+    async def generate_stream(
+        self,
+        prompt,
+        temperature=0.6,
+        top_p=0.95,
+        top_k=50,
+        max_new_tokens=128,
+        stop_str=None,
+        stop_token_ids=None,
+        repetition_penalty=1.0
+    ):
+        results_generator = self.engine(prompt, temperature=temperature, top_p=top_p, top_k=top_k, max_new_tokens=max_new_tokens,
+                                        stop_str=stop_str, stop_token_ids=stop_token_ids, repetition_penalty=repetition_penalty)
+
+        async for generated_text in results_generator:
+            yield generated_text
+
+    def predict(
+        self,
+        lora_path: str = Input(
+            description="Path to .zip of LoRA weights.", default="https://pub-df34620a84bb4c0683fae07a260df1ea.r2.dev/sql.zip"),
+        prompt: str = Input(description="Prompt to send to CodeLlama."),
+        max_new_tokens: int = Input(
+            description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
+            ge=1,
+            default=128,
+        ),
+        temperature: float = Input(
+            description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.",
+            ge=0.01,
+            le=5,
+            default=0.75,
+        ),
+        top_p: float = Input(
+            description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens",
+            ge=0.0,
+            le=1.0,
+            default=0.9,
+        ),
+        top_k: int = Input(
+            description="When decoding text, samples from the top k most likely tokens; lower to ignore less likely tokens",
+            ge=0,
+            default=50,
+        ),
+        stop_sequences: str = Input(
+            description="A comma-separated list of sequences to stop generation at. For example, '<end>,<stop>' will stop generation at the first instance of 'end' or '<stop>'.",
+            default=None,
+        ),
+        debug: bool = Input(
+            description="provide debugging output in logs", default=False
+        ),
+    ) -> ConcatenateIterator[str]:
+
+        if lora_path:
+            adapter_config, adapter_model = self.get_lora(
+                replicate_weights=lora_path)
+            lora = self.engine.load_lora(adapter_model, adapter_config)
+            self.engine.set_lora(lora)
+        if stop_sequences:
+            stop_sequences = stop_sequences.split(",")
+
+        loop = asyncio.get_event_loop()
+
+        start_time = time.time()
+        gen = self.generate_stream(
+            prompt,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            max_new_tokens=max_new_tokens,
+            stop_str=stop_sequences,
+            repetition_penalty=1.0
+        )
+
+        generated_text = ""
+        num_tokens = 0
+        while True:
+            try:
+                generated_tokens = loop.run_until_complete(gen.__anext__())
+                num_tokens += 1
+                yield generated_tokens
+                generated_text += generated_tokens
+            except StopAsyncIteration:
+                end_time = time.time()
+                break
+
+        generation_speed = num_tokens / (end_time - start_time)
+        print(
+            f"Generated {num_tokens} tokens in {end_time - start_time:.3f} seconds ({generation_speed:.3f} tokens per second)")
+        return generated_text
