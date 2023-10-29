361efde96bcc513411f5fc8642cc370cee89df91
dan <dan@replicate.com>
2023-10-29 23:01:44 +0000

    now with mlc vllm

---
+++ src/inference_engines/mlc_vllm_engine.py
@@ -0,0 +1,83 @@
+import gc
+from typing import Any, Optional, List
+
+import torch
+import os
+
+from .engine import Engine
+from .vllm_engine import vLLMEngine
+from .mlc_engine import MLCEngine
+
+
+class MLCVLLMEngine(Engine):
+    """
+    MLC for base models, vllm for fine-tunes. 
+    """
+
+    def __init__(self, vllm_args: dict, mlc_args: dict) -> None:
+        # checks for old style loras & if this is booted as a fine-tuneable hotswap
+        if 'COG_WEIGHTS' in os.environ or ('REPLICATE_HOTSWAP' in os.environ and os.environ['REPLICATE_HOTSWAP'] == "1"):
+            self.engine = vLLMEngine(**vllm_args)
+        else:
+            self.engine = MLCEngine(**mlc_args)
+            self.vllm_args = vllm_args
+
+
+    def load_lora(self, lora_data:dict) -> Any:
+        """
+        loads a lora from files into the format that this particular engine expects. DOES NOT prepare the engine for inference.
+        lora_data is a dictionary of file names & references from the zip file
+        """
+        if isinstance(self.engine, MLCEngine):
+            # Really we should never need to do this.
+            print("Transitioning from MLC to vLLM")
+            del self.cm
+            del self.tokenizer
+
+            gc.collect()
+            torch.cuda.empty_cache()
+            self.engine = vLLMEngine(**self.vllm_args)
+        
+        return self.engine.load_lora(lora_data)
+        
+
+    def is_lora_active(self) -> bool:
+        """
+        Returns True if the engine is currently configured to use a lora, False otherwise.
+        """
+        if isinstance(self.engine, vLLMEngine):
+            return self.engine.is_lora_active()
+        return False
+
+    def set_lora(self, lora: Any) -> None:
+        """
+        Given a loaded lora (created w/ load_lora), configures the engine to use that lora in combination with the loaded base weights.
+        """
+        if isinstance(self.engine, MLCEngine):
+            raise Exception("Loras not supported with MLC Engine! Invalid state reached.")
+        self.engine.set_lora(lora)
+
+    def delete_lora(self) -> None:
+        self.engine.delete_lora()
+
+
+    def __call__(self, 
+                 prompt,
+                 max_new_tokens:int =128,
+                 min_new_tokens:int =-1,
+                 temperature:float =0.75,
+                 top_p:float =0.9,
+                 top_k:int =50,
+                 stop_sequences: Optional[List[str]] = None,
+                 **kwargs):
+        print(f"MLC: {isinstance(self.engine, MLCEngine)}")
+        gen = self.engine(prompt, 
+                          max_new_tokens=max_new_tokens, 
+                          min_new_tokens=min_new_tokens,
+                          temperature=temperature,
+                          top_p=top_p,
+                          top_k=top_k,
+                          stop_sequences=stop_sequences,
+                          **kwargs)
+        for val in gen:
+            yield val
