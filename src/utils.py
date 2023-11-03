31b9b4a6e50b67dd32999ac068884b3a1b8a0f1d
Dan Nelson <dan@replicate.com>
2023-11-02 17:39:13 -0700

    ceiling on pget (#57)

--- src/utils.py
+++ src/utils.py
@@ -79,14 +79,14 @@ def check_files_exist(remote_files, local_path):
     return missing_files
 
 
-async def download_file_with_pget(remote_path, dest_path):
+async def download_file_with_pget(remote_path, dest_path, pget_concurrency='10'):
     # Create the subprocess
     print("Downloading ", remote_path)
     if remote_path.endswith("json"):
         info = "%{filename_effective} took %{time_total}s (%{speed_download} bytes/sec)\n"
         args = ["curl", "-w", info, "-sLo", dest_path, remote_path]
     else:
-        args = ["pget", remote_path, dest_path]
+        args = ["pget", "-c", pget_concurrency, remote_path, dest_path]
     process = await asyncio.create_subprocess_exec(
         *args,
         stdout=asyncio.subprocess.PIPE,
@@ -104,10 +104,10 @@ async def download_file_with_pget(remote_path, dest_path):
         print(f"[stderr]\n{stderr.decode()}")
 
 
-async def download_files_with_pget(remote_path, path, files):
+async def download_files_with_pget(remote_path, path, files, pget_concurrency):
     await asyncio.gather(
         *(
-            download_file_with_pget(f"{remote_path}/{file}", f"{path}/{file}")
+            download_file_with_pget(f"{remote_path}/{file}", f"{path}/{file}", str(pget_concurrency))
             for file in files
         )
     )
@@ -157,12 +157,19 @@ def maybe_download_with_pget(
 
         if len(missing_files) > 0:
             print("Downloading weights...")
+            
+            num_concurrent_connections = 40
             st = time.time()
-            if logger:
-                logger.info(f"Downloading {missing_files} from {remote_path} to {path}")
-            asyncio.run(download_files_with_pget(remote_path, path, missing_files))
-            if logger:
-                logger.info(f"Finished download")
+            for batch_start in range(0, len(missing_files), num_concurrent_connections):
+                batch_end = min(batch_start + num_concurrent_connections, len(missing_files))
+                missing_file_batch = missing_files[batch_start:batch_end]
+                pget_concurrency = str(max(1, int(num_concurrent_connections/ len(missing_file_batch))))
+                if logger:
+                    logger.info(f"Downloading {missing_files} from {remote_path} to {path}")
+                asyncio.run(download_files_with_pget(remote_path, path, missing_file_batch
+                                                     , pget_concurrency))
+                if logger:
+                    logger.info(f"Finished download")
             print(f"Finished download in {time.time() - st:.2f}s")
 
     return path
