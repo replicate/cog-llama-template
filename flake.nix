f4944b83a88c8651af27118b43f20ea6b094f9b8
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-11-08 17:00:45 +0100

    Fix pget, cacert, train

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    cognix.url = "github:technillogue/cognix?rev=4017831c8cdbb8bc3d32eec0887bcae5e5815639";
+    cognix.url = "github:datakami/cognix";
   };
 
   outputs = { self, cognix }@inputs: cognix.lib.singleCognixFlake inputs "insert-name-here";
