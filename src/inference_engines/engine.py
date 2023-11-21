f54846d7019719979dcd55a9189569477b7e01da
technillogue <technillogue@gmail.com>
2023-11-20 19:09:30 -0500

    new pget i guess? (#73)
    * new pget i guess?
    * max conn per host 100

--- src/inference_engines/engine.py
+++ src/inference_engines/engine.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 from src.config_utils import Weights
-from src import download
+from src.utils import maybe_download_with_pget
 
 
 class Engine(ABC):
@@ -12,9 +12,8 @@ class Engine(ABC):
     """
 
     def load_weights(self, weights: Weights):
-        downloader = download.global_downloader or download.Downloader()
         start = time.time()
-        downloader.sync_maybe_download_files(
+        maybe_download_with_pget(
             weights.local_path, weights.remote_path, weights.remote_files
         )
         print(f"downloading weights took {time.time() - start:.3f}s")
