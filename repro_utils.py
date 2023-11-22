87499478c1036f625d6ff6f5d777b018ed3a7050
technillogue <technillogue@gmail.com>
2023-11-22 23:24:20 +0000

    repro

---
+++ repro_utils.py
@@ -0,0 +1,119 @@
+import asyncio
+import dataclasses
+import os
+import typing as tp
+
+
+def get_loop() -> asyncio.AbstractEventLoop:
+    try:
+        return asyncio.get_running_loop()
+    except RuntimeError:
+        return asyncio.new_event_loop()
+
+
+async def download_files_with_pget(
+    remote_path: str, path: str, files: list[str]
+) -> None:
+    download_jobs = "\n".join(f"{remote_path}/{f} {path}/{f}" for f in files)
+    args = ["pget", "multifile", "-", "-f", "--max-conn-per-host", "100"]
+    process = await asyncio.create_subprocess_exec(*args, stdin=-1, close_fds=True)
+    # Wait for the subprocess to finish
+    await process.communicate(download_jobs.encode())
+
+
+def check_files_exist(remote_files: list[str], local_path: str) -> list[str]:
+    # Get the list of local file names
+    local_files = os.listdir(local_path)
+
+    # Check if each remote file exists in the local directory
+    missing_files = list(set(remote_files) - set(local_files))
+
+    return missing_files
+
+
+def maybe_download_with_pget(
+    path: str,
+    remote_path: tp.Optional[str] = None,
+    remote_filenames: tp.Optional[list[str]] = None,
+):
+    """
+    Downloads files from remote_path to path if they are not present in path. File paths are constructed
+    by concatenating remote_path and remote_filenames. If remote_path is None, files are not downloaded.
+
+    Args:
+        path (str): Path to the directory where files should be downloaded
+        remote_path (str): Path to the directory where files should be downloaded from
+        remote_filenames (List[str]): List of file names to download
+
+    Returns:
+        path (str): Path to the directory where files were downloaded
+
+    Example:
+
+        maybe_download_with_pget(
+            path="models/roberta-base",
+            remote_path="gs://my-bucket/models/roberta-base",
+            remote_filenames=["config.json", "pytorch_model.bin", "tokenizer.json", "vocab.json"],
+        )
+    """
+    for _path in remote_filenames:
+        path_directory = os.path.dirname(_path)
+        if path_directory:
+            path_directory = os.path.join(path, path_directory)
+            os.makedirs(path_directory, exist_ok=True)
+
+    if remote_path:
+        remote_path = remote_path.rstrip("/")
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+            missing_files = remote_filenames or []
+        else:
+            missing_files = check_files_exist(remote_filenames or [], path)
+        get_loop().run_until_complete(
+            download_files_with_pget(remote_path, path, missing_files)
+        )
+
+    return path
+
+
+def get_mlc_file_list(model_name: str, n_shards: int):
+    files_to_download = [
+        f"params/params_shard_{shard_idx}.bin" for shard_idx in range(n_shards)
+    ]
+
+    files_to_download += [
+        f"{model_name}-cuda.so",
+        "mod_cache_before_build.pkl",
+        "params/mlc-chat-config.json",
+        "params/ndarray-cache.json",
+        "params/tokenizer.json",
+        "params/tokenizer_config.json",
+        "params/tokenizer.model",
+        "params/config.json",
+    ]
+    return files_to_download
+
+
+@dataclasses.dataclass
+class Weights:
+    local_path: str
+    remote_path: str
+    remote_files: list[str]
+
+
+def mlc_kwargs(
+    weights: Weights,
+    is_chat: bool,
+    num_shards: int = 1,
+    tokenizer_path: str = None,
+    config_overrides: None | dict = None,
+):
+    mlc_default = {
+        "weights": weights,
+        "tokenizer_path": tokenizer_path,
+        "is_chat": is_chat,
+        "num_shards": num_shards,
+    }
+    if config_overrides:
+        mlc_default.update(config_overrides)
+    return mlc_default
---
+++ reproduce.py
@@ -0,0 +1,141 @@
+import os
+from mlc_chat import ChatConfig, ChatModule, ConvConfig, GenerationConfig
+from transformers import AutoTokenizer
+from repro_utils import Weights, get_mlc_file_list, maybe_download_with_pget, mlc_kwargs
+
+mlc_file_list = get_mlc_file_list(
+    model_name="Llama-2-70b-chat-hf-q4f16_1", n_shards=483
+)
+MODEL_NAME = "llama-2-70b-chat-hf-mlc"
+
+mlc_weights = Weights(
+    local_path=f"models/{MODEL_NAME}/model_artifacts/default_inference_weights",
+    remote_path="https://weights.replicate.delivery/default/llama-2-70b-chat-hf-mlc",
+    remote_files=mlc_file_list,
+)
+
+ENGINE_KWARGS = mlc_kwargs(mlc_weights, is_chat=False)
+
+
+class MLCEngine:
+    def __init__(
+        self,
+        weights: Weights,
+        num_shards: int = 1,
+        is_chat: bool = False,
+        tokenizer_path: os.PathLike = None,
+    ) -> None:
+        weights_path = mlc_weights.local_path
+        self.num_shards = num_shards
+
+        self.conv_template = "LM"
+        self.stop_str = "[INST]"
+        self.stop_tokens = [
+            2,
+        ]
+        self.add_bos = True
+        maybe_download_with_pget(
+            weights.local_path, weights.remote_path, weights.remote_files
+        )
+
+        conv_config = ConvConfig(
+            stop_tokens=self.stop_tokens, add_bos=self.add_bos, stop_str=self.stop_str
+        )
+        chat_config = ChatConfig(
+            conv_config=conv_config,
+            conv_template=self.conv_template,
+            num_shards=self.num_shards,
+        )
+
+        model_path = os.path.join(weights_path, "params")
+        self.cm = ChatModule(model=model_path, chat_config=chat_config, device="cuda")
+
+        tokenizer_path = os.path.join(weights_path, "params")
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    def __call__(
+        self,
+        prompt: str,
+        max_new_tokens: int,
+        temperature: float,
+        top_p: float,
+        top_k: int,
+        *args,
+        stop_sequences: str | list[str] = None,
+        stop_token_ids: list[int] = [],
+        repetition_penalty: float = 1.0,
+        incremental_generation: bool = True,
+        **kwargs,
+    ):
+        """
+        Given a prompt, runs generation on the language model with vLLM.
+
+        Args:
+        - prompt (str): the prompt to give the model.
+        - max_new_tokens (int): the maximum number of new tokens to generate.
+        - temperature (float): the parameter to anneal the sampling distribution with.
+        - top_p (float): the amount to truncate the sampling distribution by.
+        - top_k (int): the number of tokens to truncate the sampling distribution by.
+        - stop_sequences (str | list[str]): the string to stop generation at.
+        - stop_token_ids (list[str]): a list of token ids to stop generation at.
+        - frequency_penalty (float): the amount to penalize tokens that have already been generated, higher values penalize more.
+        - incremental_generation: whether to yield the entire generated sequence or the next generated token at each step.
+
+        Yields:
+        - generated_text (str): the generated text, or next token, depending on the value of `incremental_generation`.
+        """
+
+        if top_k is not None and top_k > 0:
+            raise ValueError(
+                "top_k is currently not supported by our generation engine."
+            )
+
+        stop_token_ids += self.stop_tokens
+        # stop_sequences = [self.stop_str] + stop_sequences
+
+        # TODO (Moin): add support for the system prompt on chat models
+        conv_config = ConvConfig(
+            stop_tokens=stop_token_ids, add_bos=self.add_bos, stop_str=stop_sequences
+        )
+        chat_config = ChatConfig(
+            temperature=temperature,
+            repetition_penalty=repetition_penalty,
+            top_p=top_p,
+            max_gen_len=max_new_tokens,
+            mean_gen_len=max_new_tokens,
+            conv_config=conv_config,
+            conv_template=self.conv_template,
+            num_shards=self.num_shards,
+        )
+        self.cm.reset_chat(chat_config)
+
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            repetition_penalty=repetition_penalty,
+            top_p=top_p,
+            max_gen_len=max_new_tokens,
+        )
+        self.cm._prefill(input=prompt, generation_config=generation_config)
+
+        min_new_tokens = kwargs.pop("min_new_tokens", None)
+        if min_new_tokens is not None and min_new_tokens > -1:
+            raise ValueError(
+                "min_new_tokens is currently not supported by MLC's engine."
+            )
+
+        if len(kwargs) > 0:
+            raise ValueError(f"Unknown keyword arguments: {', '.join(kwargs.keys())}")
+
+        generation_length = 0
+        while True:
+            if self.cm._stopped():
+                break
+            self.cm._decode(generation_config=generation_config)
+            out = self.cm._get_message()
+            yield out[generation_length:]
+            generation_length = len(out)
+
+
+if __name__ == "__main__":
+    engine = MLCEngine(**ENGINE_KWARGS)
+    print(next(engine("hi")))
